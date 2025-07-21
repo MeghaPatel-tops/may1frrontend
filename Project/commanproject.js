068cdebbe98c42514c6de031694677687f579410
Megha <meghapatel1.tops@gmail.com>
2025-07-21 16:00:59 +0530

    backup

--- Project/commanproject.js
+++ Project/commanproject.js
@@ -6,13 +6,35 @@
       <div class="panel panel-primary">
         <div class="panel-heading">${productname}</div>
         <div class="panel-body"><img src="${item.image}" class="img-responsive" style="width:100%;height:200px" alt="Image"></div>
-        <div class="panel-footer">Price:${item.price} <button class='me-0 btn btn-primary'>Add To Cart</button></div>
+        <div class="panel-footer">Price:${item.price} <button class='me-0 btn btn-primary' onclick="addtocart(${item.id})">Add To Cart</button></div>
       </div>
     </div>`;
 
            
         })
         document.getElementById("pdata").innerHTML = str;
+    }
+
+    function addtocart(pid){
+         alert(pid);
+          let user = localStorage.getItem('sessionUser');
+          user = JSON.parse(user);
+          
+
+          if(localStorage.getItem("cart-"+user.email)){
+               let cartArray = JSON.parse(localStorage.getItem("cart-" + user.email)) || [];;
+               alert(cartArray);
+               cartArray.push(pid);
+               localStorage.setItem("cart-"+user.email,JSON.stringify(cartArray));
+          }
+          else{
+            let cartItem = [];
+            cartItem.push(pid);
+            alert(cartItem);
+            localStorage.setItem("cart-"+user.email,JSON.stringify(cartItem));
+          }
+          window.location="index.html";
+
     }
 
     async function getProduct(){
@@ -74,12 +96,20 @@
      
           let user = localStorage.getItem('sessionUser');
           user = JSON.parse(user);
-          let str= `<li><a href=""><span class="glyphicon glyphicon glyphicon-user">
+          if(localStorage.getItem("cart-"+user.email)){
+               let cartArray = JSON.parse(localStorage.getItem("cart-" + user.email)) || [];;
+                let count=cartArray.length;
+          
+          let str= `
+           <li><a href="cart.html"><span class="glyphicon glyphicon-shopping-cart"></span><super>${count}</super></a></li>
+          <li><a href=""><span class="glyphicon glyphicon glyphicon-user">
           </span> ${user.uname}</a></li>
           <li><button onclick='logout()'><span class="glyphicon 	glyphicon glyphicon-off"></button></li>
+         
           `;
           
           document.getElementById('loginItem').innerHTML=str;
+          }
       }
       else{
            let str= `<li><a href="login.html">Login
@@ -96,5 +126,45 @@
           window.location="index.html";
     }
 
-    window.onload = loading();
+   async function getCartData(){
+         let user = localStorage.getItem('sessionUser');
+          user = JSON.parse(user);
+           if(localStorage.getItem("cart-"+user.email)){
+               let cartArray = JSON.parse(localStorage.getItem("cart-" + user.email)) || [];
+               
+                try {
+                const product =  await fetch('https://fakestoreapi.com/products');
+                const productData = await product.json();
+                 //const cartProductIds = cartArray.map(item => item.id);
+               //   alert(cartProductIds);
+
+                  const newProductArray = productData.filter(product =>
+                     cartArray.includes(product.id)
+                  );
+                console.log(newProductArray);
+                cartCreate(newProductArray);
+               } catch (error) {
+                  console.log(error);
+               }
+              
+          }
+          
+    }
+
+    function cartCreate(cartData){
+         let str="";
+         cartData.map((item)=>{
+            str+= `<tr>
+        <td>${item.title}</td>
+        <td>${item.price}</td>
+        <td><img src="${item.image}" alt="" height="50px" width="50px"></td>
+        
+      </tr>`;
+      document.getElementById("cartData").innerHTML=str;
+         })
+    }
+
+   
+
+    
      
